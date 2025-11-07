8b697b51394156796747e879d26a2571a86e9684
UrielhRibeiro <uriel.h.ribeiro@gmail.com>
2025-11-07 09:18:30 -0300

    feat: adição vercel.json

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,21 +12,23 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response) => {
     try {
-    
-        const loginResponse = await authService.loginUser(req.body); 
+        // 1. Pega os dados do body
+        const { email, password } = req.body;
 
+        // Ele vai retornar o token ou vai 'lança' (throw) um erro 401
+        const loginResponse = await authService.loginUser({ email, password }); 
+
+        // 3. Se o service NÃO 'lançou' um erro, o login foi um sucesso.
         return res.status(200).json(loginResponse);
 
     } catch (error: any) {
-    
+        
         const status = error.status || 500;
         const message = error.message || 'Erro interno do servidor.';
-        
         
         return res.status(status).json({ message });
     }
 };
-
 export const getProtectedData = (req: any, res: any) => {
   res.json({ message: "Rota de teste OK" });
 };
